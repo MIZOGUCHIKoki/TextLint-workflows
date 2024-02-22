782d1379ece12b1c36ae01567b78d80b15c10b3d
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2024-02-22 15:11:35 +0900

    upda

--- configuration-files/lint.sh
+++ configuration-files/lint.sh
@@ -1,15 +1,15 @@
 #/bin/zsh
 
-if [ -f ./.lint_ja.sh ]; then
+if [ -f lint_ja.sh ]; then
   echo "execute lint_ja.sh"
-  ./.lint_ja.sh
+  zsh lint_ja.sh >> textlint_ja.txt
 else
   echo "lint_ja.sh not found"
 fi
 
-if [ -f ./.lint_en.sh ]; then
+if [ -f lint_en.sh ]; then
   echo "execute lint_en.sh"
-  ./.lint_en.sh
+  zsh lint_en.sh >> textlint_en.txt
 else
   echo "lint_en.sh not found"
 fi
